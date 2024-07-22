d4ce118d782004159318713e684ce34279abdf10
Gerti23 <gertibajo17@gmail.com>
2024-07-22 10:19:17 +0000

    update

--- main.h
+++ main.h
@@ -3,10 +3,15 @@
 
 #include <stdarg.h>
 #include <unistd.h>
-
+int _putchar(char c);
+typedef struct format_specifier
+{
+    char *specifier;
+    int (*print_func)(va_list);
+} format_specifier_t;
 int print_char(va_list args);
 int print_string(va_list args);
 int print_int(va_list args);
-int _putchar(char c);
+
 
 #endif
